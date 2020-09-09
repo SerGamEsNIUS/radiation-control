2624704d67c13f4563657394a516cf6564173ff5
SerGamEsNIUS <chemist-sergeev98@yandex.ru>
2020-09-09 16:20:28 +0300

    Hi

---
+++ src/main/java/by/serg/radiationcontrol/RadiationControlApplication.java
@@ -0,0 +1,13 @@
+package by.serg.radiationcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadiationControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadiationControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/serg/radiationcontrol/RadiationControlApplicationTests.java
@@ -0,0 +1,13 @@
+package by.serg.radiationcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadiationControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
